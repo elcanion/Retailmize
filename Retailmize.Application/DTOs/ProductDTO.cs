6d1d8cd787562d67b66a6eddf7e43c7db4905077
elcanion <elcaniondev@gmail.com>
2024-09-03 12:51:35 -0300

    Added DTOs for category and product

---
+++ Retailmize.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,50 @@
+﻿using Retailmize.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Stock required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product image")]
+        public string Image { get; set; }
+
+        public Category Category { get; set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+    }
+}
