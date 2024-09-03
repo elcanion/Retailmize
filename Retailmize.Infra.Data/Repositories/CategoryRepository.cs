6f17dbbcc81f368b17ceba7c383e5f161f24c4e3
elcanion <elcaniondev@gmail.com>
2024-09-03 10:44:30 -0300

    Added repository classes (they aren't implemented yet)

---
+++ Retailmize.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using Retailmize.Domain.Entities;
+using Retailmize.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public Task<Product> Create(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetById(int? Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> Remove(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Retailmize.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Retailmize.Domain.Entities;
+using Retailmize.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<Product> Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductCategory(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> Remove(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
