2de5753a47cbbc3af510a1f749edf5dd38b6b8d5
elcanion <elcaniondev@gmail.com>
2024-09-02 21:21:24 -0300

    First commit

---
+++ Retailmize.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Retailmize.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Retailmize.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Retailmize.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
