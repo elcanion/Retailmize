68aa520b037d5ed448e1340fe438667840d06017
elcanion <elcaniondev@gmail.com>
2024-09-03 11:19:39 -0300

    Added dependency injection for repositories

---
+++ Retailmize.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Retailmize.Domain.Interfaces;
+using Retailmize.Infra.Data.Context;
+using Retailmize.Infra.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure
+            (this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+            );
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
