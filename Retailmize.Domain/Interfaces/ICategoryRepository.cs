cd23e0e0bef7fc89ab608cef3a202f2ae6d5d129
elcanion <elcaniondev@gmail.com>
2024-09-02 22:53:56 -0300

    Added product and category repository interfaces

---
+++ Retailmize.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Retailmize.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Task<Category> GetById(int? Id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ Retailmize.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Retailmize.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int? id);
+        Task<Product> GetProductCategory(int? id);
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(Product product);
+    }
+}
