dd9430f74e121c319962870815ccf1ca70ff7d4e
elcanion <elcaniondev@gmail.com>
2024-09-03 13:01:05 -0300

    Added profile for category and product DTOs

---
+++ Retailmize.Application/Mappings/DomainToDTOMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Retailmize.Application.DTOs;
+using Retailmize.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Application.Mappings
+{
+    public class DomainToDTOMapping : Profile
+    {
+        public DomainToDTOMapping() 
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
