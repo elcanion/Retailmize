299fcfad39e4ba2ace851a7452df274caeaeb81e
elcanion <elcaniondev@gmail.com>
2024-09-04 17:56:50 -0300

    Added services for categories and products

---
+++ Retailmize.Application/Services/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Retailmize.Application.DTOs;
+using Retailmize.Application.Interfaces;
+using Retailmize.Domain.Entities;
+using Retailmize.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailmize.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetAll()
+        {
+            var productsEntity = await _productRepository.GetAll();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productEntity = await _productRepository.GetById(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategory(int? id)
+        {
+            var productEntity = await _productRepository.GetProductCategory(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.Create(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.Update(productEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity = _productRepository.GetById(id).Result;
+            await _productRepository.Remove(productEntity);
+        }
+    }
+}
