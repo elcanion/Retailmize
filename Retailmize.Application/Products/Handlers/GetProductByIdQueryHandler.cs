eca1936980f01f8180c29ec3e0f8f072f033ce88
elcanion <elcaniondev@gmail.com>
2024-09-15 16:11:33 -0300

    Added handlers for product queries

---
+++ Retailmize.Application/Products/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Retailmize.Application.Products.Queries;
+using Retailmize.Domain.Entities;
+using Retailmize.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Retailmize.Application.Products.Handlers
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetById(query.Id);
+        }
+    }
+}
---
+++ Retailmize.Application/Products/Handlers/GetProductsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Retailmize.Application.Products.Queries;
+using Retailmize.Domain.Entities;
+using Retailmize.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Retailmize.Application.Products.Handlers
+{
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<Product>>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetAll();
+        }
+    }
+}
