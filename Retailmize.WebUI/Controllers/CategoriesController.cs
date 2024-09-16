f039831dfcf4c6c2a08116f55436198667ea838a
elcanion <elcaniondev@gmail.com>
2024-09-15 21:18:22 -0300

    Added actions and views for categories

--- Retailmize.WebUI/Controllers/CategoriesController.cs
+++ Retailmize.WebUI/Controllers/CategoriesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Retailmize.Application.DTOs;
 using Retailmize.Application.Interfaces;
 using Retailmize.Application.Services;
+using System;
 using System.Threading.Tasks;
 
 namespace Retailmize.WebUI.Controllers
@@ -24,6 +26,68 @@ namespace Retailmize.WebUI.Controllers
         public async Task<IActionResult> Create()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var categoryDTO = await _categoryService.GetById(id);
+            if (categoryDTO == null) return NotFound();
+            return View(categoryDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(categoryDTO);
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            var categoryDTO = await _categoryService.GetById(id);
+            if (categoryDTO == null) return NotFound(); 
+            return View(categoryDTO);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletionConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDTO = await _categoryService.GetById(id);
+            if (categoryDTO == null) return NotFound(); 
+            return View(categoryDTO);
         }
     }
 }
