bba55739b5ecbaca7fe54014e4b9a34a1f0630fc
elcanion <elcaniondev@gmail.com>
2024-09-16 15:11:44 -0300

    Added controllers for login and registration.

--- Retailmize.WebUI/Controllers/AccountController.cs
+++ Retailmize.WebUI/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Retailmize.Domain.Account;
+using Retailmize.WebUI.ViewModels;
+using System.Threading.Tasks;
 
 namespace Retailmize.WebUI.Controllers
 {
@@ -9,6 +11,59 @@ namespace Retailmize.WebUI.Controllers
         public AccountController(IAuthenticate authentication)
         {
             _authentication = authentication;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authentication.Authenticate(model.Email, model.Password);
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var result = await _authentication.Register(model.Email, model.Password);
+            if (result)
+                return Redirect("/");
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid registration attempt");
+                return View(model);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _authentication.Logout();
+            return Redirect("/Account/Login");
         }
     }
 }
