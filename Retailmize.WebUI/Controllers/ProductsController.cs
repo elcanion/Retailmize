222aa3fd781aa8e8425433d55062db6ec0fcc1f3
elcanion <elcaniondev@gmail.com>
2024-09-15 22:24:24 -0300

    Added actions and views for products.

--- Retailmize.WebUI/Controllers/ProductsController.cs
+++ Retailmize.WebUI/Controllers/ProductsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Retailmize.Application.DTOs;
 using Retailmize.Application.Interfaces;
+using Retailmize.Application.Services;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Retailmize.WebUI.Controllers
@@ -7,9 +12,13 @@ namespace Retailmize.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
+        public ProductsController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment environment)
         {
             _productService = productService;
+            _categoryService = categoryService;
+            _environment = environment;
         }
 
         [HttpGet]
@@ -17,6 +26,68 @@ namespace Retailmize.WebUI.Controllers
         {
             var products = await _productService.GetAll();
             return View(products);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetAll(), "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDTO = await _productService.GetById(id);
+            if (productDTO == null) return NotFound();
+            var categories = await _categoryService.GetAll();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDTO.CategoryId);
+            return View(productDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDTO);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDTO = await _productService.GetById(id);
+            if (productDTO == null) return NotFound();
+            return View(productDTO);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletionConfirmed(int id)
+        {
+            await _productService.Remove(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDTO = await _productService.GetById(id);
+            if (productDTO == null) return NotFound();
+            var wwwroot = _environment.WebRootPath;
+            var image = Path.Combine(wwwroot, "images\\" + productDTO.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+            return View(productDTO);
         }
     }
 }
