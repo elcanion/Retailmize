2d5d1f0957f2179f97f2c1aeed8e07228b2dac63
elcanion <elcaniondev@gmail.com>
2024-09-17 12:01:07 -0300

    Added API models for registration and login

---
+++ Retailmize.API/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Retailmize.API.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, ErrorMessage = "{0} must be at least {2} an max {1} characters long.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
